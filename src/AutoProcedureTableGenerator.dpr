bc3b4ff7bf0ec2fccaf573d71d46091ad700a92f
Alexandr Pankratiew <sasha.pankratiew@gmail.com>
2018-03-08 16:34:00 +0300

    Upload First version

---
+++ src/AutoProcedureTableGenerator.dpr
@@ -0,0 +1,14 @@
+program AutoProcedureTableGenerator;
+
+uses
+  Vcl.Forms,
+  genTable in 'genTable.pas' {TableGenForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TTableGenForm, TableGenForm);
+  Application.Run;
+end.
